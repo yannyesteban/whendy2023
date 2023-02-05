4ec95c1e225e55aad83ba89627b8ba966550c2b0
Yanny Esteban <yannyesteban@hotmail.com>
2023-02-05 13:12:47 -0400

    ok

--- engine/cmd/logic/logic.go
+++ engine/cmd/logic/logic.go
@@ -7,15 +7,14 @@ import (
 )
 
 func main() {
-	nihongo := LoadFile("texto.txt") //"日本語"
-	for index, runeValue := range nihongo {
-		fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+	source := LoadFile("texto.txt") //"日本語"
 
-	}
-	//nihongo[1]
-	r, w := utf8.DecodeRuneInString(nihongo[1:])
+	p := Parse{}
+	p.start(source)
 
-	fmt.Println(r, w, nihongo[0:2])
+	//r, w := utf8.DecodeRuneInString(nihongo[1:])
+
+	//fmt.Println(r, w, nihongo[0:2])
 }
 
 func LoadFile(name string) string {
@@ -30,4 +29,56 @@ func LoadFile(name string) string {
 
 	return str
 
+}
+
+type Parse struct {
+	text  string
+	state int
+	pos   int
+	typ   int
+}
+
+func (p *Parse) start(text string) {
+	p.text = text
+	p.pos = 0
+
+	for {
+
+		fmt.Println("len ", len(text))
+
+		rune, w := utf8.DecodeRuneInString(p.text[p.pos:])
+
+		if p.isDelimeter(rune) {
+			fmt.Printf("%#U starts at byte position %d\n", rune, w)
+
+		}
+		fmt.Printf("%#U starts at byte position %d\n", rune, w)
+		p.pos++
+		if p.pos >= len(p.text) {
+			break
+		}
+
+	}
+
+}
+
+func (p *Parse) isDelimeter(s rune) bool {
+
+	switch {
+	case s == '@':
+		p.typ = 1
+		return true
+	}
+
+	return false
+}
+
+func (p Parse) isS(s rune) bool {
+
+	switch {
+	case s == '@':
+		return true
+	}
+
+	return false
 }
